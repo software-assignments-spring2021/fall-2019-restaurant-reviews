4b82e0027463c70ec1df2ec67a36281374e91ec9
MagicShow1999 <hw1635@nyu.edu>
2019-10-28 11:38:09 -0400

    Refine restaurant component

--- restaurant-review-app/client/src/containers/Restaurants/Restaurant.js
+++ restaurant-review-app/client/src/containers/Restaurants/Restaurant.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import styles from './Restaurant.module.css';
 
+//NOTE: SHOULD INCLUDE A NAVBAR THE SAME AS IN THE LANDING PAGE.
 class Restaurant extends Component{
 
     state = {
@@ -9,13 +11,26 @@ class Restaurant extends Component{
 
         location : '5 University Pl',
 
-        dishes : 
+        dishes : [
             {
                 name:'Beef noodles',
                 rating:'4',
                 comments:['tasty beef','delicious soup']
             },
 
+            {
+                name:'Chicken noodles',
+                rating:'2',
+                comments:['broth is normal','chicken is not fresh','pricy']
+            },
+            {
+                name:'Veggie noodles',
+                rating:'3.5',
+                comments:['great ingredients','smells good']
+            }
+
+
+        ]
             // {
             //     name:'Stir-fried chicken noodles',
             //     rating:'5',
@@ -34,15 +49,30 @@ class Restaurant extends Component{
     render(){
         return(
 
-            <div className='restaurant-name'>
-                <p3>{this.state.name}</p3> 
-                <br></br>
-                <p4>{this.state.location}</p4>
-                <div className='Dish'>     
-                    {this.state.dishes.name} <br/>
-                    {this.state.dishes.location} <br/>
-                    {this.state.dishes.comments[0]} <br/>
-                    {this.state.dishes.comments[1]}<br/>
+            <div className={styles.Restaurant}>
+                <h2>{this.state.name}</h2> 
+                
+                <h4>{this.state.location}</h4>
+                <div className={styles.Dish}>     
+                    {this.state.dishes[0].name} <br/>
+                    <div className={styles.Comments}>
+                    {this.state.dishes[0].comments[0]} <br/>
+                    {this.state.dishes[0].comments[1]}<br/>
+                    </div>
+                </div>
+                <div className={styles.Dish}>     
+                    {this.state.dishes[1].name} <br/>
+                    <div className={styles.Comments}>
+                    {this.state.dishes[1].comments[0]} <br/>
+                    {this.state.dishes[1].comments[1]}<br/>
+                    </div>
+                </div>
+                <div className={styles.Dish}>     
+                    {this.state.dishes[2].name} <br/>
+                    <div className={styles.Comments}>
+                    {this.state.dishes[2].comments[0]} <br/>
+                    {this.state.dishes[2].comments[1]}<br/>
+                    </div>
                 </div>
                 {/* <MyComments>  </MyComments> */}
                 
