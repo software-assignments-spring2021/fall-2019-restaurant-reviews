ac6fb0a6c0ad997e1e320edeb8ac1649c335b6cd
MagicShow1999 <hw1635@nyu.edu>
2019-10-31 10:48:35 -0400

    reorganize restaurant schema

--- restaurant-review-app/server/routes/user.js
+++ restaurant-review-app/server/routes/user.js
@@ -78,11 +78,11 @@ router.route('/login').post([
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
-  console.log('logged in!');
+
   passport.authenticate('local', {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-    // failureRedirect: '/login',
+    failureRedirect: '/login',
     successRedirect: '/user',
     
   })(req, res, next);
