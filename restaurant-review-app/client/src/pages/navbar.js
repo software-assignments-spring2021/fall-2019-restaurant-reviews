89035fca72398eabf65f5541dd4f23c83354d08e
MagicShow1999 <hw1635@nyu.edu>
2019-11-16 12:14:42 -0500

    make login/logout work

--- restaurant-review-app/client/src/pages/navbar.js
+++ restaurant-review-app/client/src/pages/navbar.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/one-page-wonder.min.css';
 import '../App.css';
 
 
-function NavBar() {
-    
+function NavBar(props) {
+  
+    //if user logs in and the token is not expired within an hour, display PROFILE, LOG OUT on nav bar 
+    if(props.loggedin){
+      
+      return(
+        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to="/">rate the plate</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" 
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item" onClick={props.onClick}>
+                  <Link className="nav-link" to="/">Sign Out</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/userpage">Profile</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      );
+    }
     return (<nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div className="container">
       <Link className="navbar-brand" to="/">rate the plate</Link>
@@ -22,12 +47,7 @@ function NavBar() {
 
           </li>
           <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-
-               
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/userpage">My User Page</Link>
+                <Link className="nav-link" to="/login">Login</Link>  
           </li>
         </ul>
       </div>
